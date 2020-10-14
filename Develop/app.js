26608d2046779e84a672182f10ecf408cb3569fc
Thalia <thalia.rowan@gmail.com>
2020-10-14 01:10:42 -0300

    added prompts for user imputs

--- Develop/app.js
+++ Develop/app.js
@@ -9,10 +9,99 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { type } = require("os");
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'manager',
+        message: 'What is the name of your team manager?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "hat is your team manager's email address?"
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is your manager's id?"
+    },
+
+    {
+        type: 'input',
+        name: 'officeNum',
+        message: "What is your manager's office number?"
+    }
+    
+    
+]).then(function(){
+    
+inquirer.prompt([
+    {
+        type: 'list',
+        name: 'employees',
+        message: 'Would you like to add more members to the team?',
+        choices: [{name:'Engineer', value: 0}, {name:'Intern', value: 1}, {name:'I do not want to add anymore members.', value: 2}]
+    }
+]).then(function(res){
+    console.log(res)
+    if(res.employees == 0 ){
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'engineer',
+                message:'What is the name of this enginner?'
+            },
+            {
+                        
+                type: 'input',
+                name: 'email',
+                message: "What is the engineer's email address?"
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is the engineer's id?"
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: "What is the engineer's github?"
+            },
+        ])
+    } else if(res.employees == 1){
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'intern',
+                message:'What is the name of this intern?'
+            },
+            {
+                        
+                type: 'input',
+                name: 'email',
+                message: "What is the intern's email address?"
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is the intern's id?"
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: "What is the intern's github?"
+            },
+        ])
+    }
+})
+})
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
